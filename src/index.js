dec4bb6edee3922e3b0bd6fd31e44da3da85340d
Diego Martinez <diegomadeit@gmail.com>
2019-05-07 11:45:44 +0200

    Add Custom Environment Variables

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./components/App";
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "GRAPHQL_API_ENDPOINT"
+  uri: process.env.REACT_APP_GRAPHQL_API_ENDPOINT
 });
 
 ReactDOM.render(<App client={client} />, document.getElementById("root"));
